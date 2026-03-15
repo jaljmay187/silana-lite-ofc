e6a1d907dfa6bfb50d10d1ea1a820b637655a598
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2026-03-16 02:40:13 +0300

    Add files via upload

---
+++ plugins/owner-مسافه.js
@@ -0,0 +1,43 @@
+/*
+بيشيل المسافات من اسامي الملفات ويحط - عشان المسافات ممكن تعمل مشاكل 
+https://whatsapp.com/channel/0029VaJxI9uJkK7BedTH0D11
+*/
+
+import fs from 'fs';
+import path from 'path';
+
+let handler = async (m, { conn }) => {
+    const folderPath = './plugins/';
+
+    fs.readdir(folderPath, (err, files) => {
+        if (err) return m.reply('حدث خطأ أثناء قراءة المجلد.');
+
+        let renamedFiles = [];
+
+        files.forEach(file => {
+            const oldPath = path.join(folderPath, file);
+            const newFileName = file.replace(/\s+/g, '-');
+            const newPath = path.join(folderPath, newFileName);
+
+            if (oldPath !== newPath) {
+                fs.rename(oldPath, newPath, err => {
+                    if (!err) renamedFiles.push(`${file} ➜ ${newFileName}`);
+                });
+            }
+        });
+
+        setTimeout(() => {
+            if (renamedFiles.length > 0) {
+                m.reply(`تم تعديل أسماء الملفات:\n\n${renamedFiles.join('\n')}`);
+            } else {
+                m.reply('لا يوجد ملفات تحتاج للتعديل.');
+            }
+        }, 1000);
+    });
+};
+
+handler.command = ['مسافه'];
+handler.help = ['renamefiles'];
+handler.tags = ['tools'];
+
+export default handler;
